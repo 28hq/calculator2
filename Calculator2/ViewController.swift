da963490ad7d930a0c406284b8bfa8c9c6e36533
AziziKhalid <azizi@brainstorm.pro>
2015-05-13 12:23:34 +0800

    Initial Commit

---
+++ Calculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator2
+//
+//  Created by Azizi Khalid on 13/5/15.
+//  Copyright (c) 2015 Brainstorm Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
